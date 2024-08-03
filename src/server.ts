76e3102a5797a50087876e4c69b786938cd3c44b
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-08-02 22:56:06 -0300

    chore: update eslint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-
-import fastify from "fastify";
+import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { Env } from '@/env/env'
 
-import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
-import { createUser } from "./routers/user";
-
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { createUser } from './routers/user'
 
 const app = fastify()
 
@@ -14,16 +15,16 @@ app.setSerializerCompiler(serializerCompiler)
 
 app.register(cors, {
   origin: Env.WEB_BASE_URL,
-  methods: ["GET", "POST", "PATCH", "DELETE", "PUT"]
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
 })
 
 app.register(createUser)
 
-
-app.listen({
-  host: '0.0.0.0',
-  port: Env.PORT
-}).then(() => {
-  console.log(`Running http://localhost:${Env.PORT}`);
-
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: Env.PORT,
+  })
+  .then(() => {
+    console.log(`Running http://localhost:${Env.PORT}`)
+  })
